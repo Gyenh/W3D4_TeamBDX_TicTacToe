8ccdddab75e31900ce9fa0be1e1fbdd914db8f4f
DaTikh <baptiste.rogeon@gmail.com>
2018-04-19 19:01:06 +0200

    Update TicTacToe.rb

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,8 +1,8 @@
 require 'color_text'
-require_relative 'Classes/Board'        # On effectue le lien avec le fichier de chaque Classe
+require_relative 'Classes/Board'       # On effectue le lien avec le fichier de chaque Classe.
 require_relative 'Classes/BoardCase'
 require_relative 'Classes/Game'
 require_relative 'Classes/Player'
 
-game = Game.new                         # On crée une nouvelle partie.
-game.go                                 # On lance le jeu !!
+game = Game.new                        # On crée une nouvelle partie.
+game.go                                # On lance le jeu !!
