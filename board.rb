36c6c6657d11d9a451a321fc5b9d13048effeeba
iMassim0 <i.max@live.fr>
2018-04-19 14:29:46 +0200

    Save

--- board.rb
+++ board.rb
@@ -1,27 +1,31 @@
 class Player
 
 @@player = 0
+@@mark = ""
 
   attr_accessor :pname, :pmark, :victory
 
-  def initialize()
+  def initialize
     @pname = ""
     @pmark = ""
     @victory = false
     @@player += 1
   end
 
-  def infos()
+  def infos
     puts "\nPlease, put the player name :"
-    @pname = (gets.chomp).to_s
-    if @@player == 1
+    @pname = ((gets.chomp).to_s).capitalize
+    if @@player == 1 && @@mark == ""
       puts "\nWhich mark do you want ? X or O ?"
       while @pmark != "X" && @pmark != "O" do
          @pmark = (gets.chomp).to_s.capitalize
       end
-    else
-      p_one[@pmark] == "X" ? @pmark = "O" : @pmark = "X"
+    elsif @@mark == "X"
+      @pmark = "O"
+    elsif @@mark == "O"
+      @pmark = "X"
     end
+    @@mark = @pmark
     puts "#{@pname} jouera avec la marque #{@pmark} !"
   end
 
@@ -31,9 +35,36 @@ end
 
 class Game
 
+  def initialize
+    #TO DO : créé 2 joueurs, créé un board
+    puts "Welcome to the BDX Tic-Tac-Toe ! Have fun with our progZ !"
+    # Créé 2 joueurs
+    @p_one = Player.new
+    @p_one.infos
+    @p_two = Player.new
+    @p_two.infos
+    # créé un board de game de 9 cases
+    @yaya = Board.new
+
+  end
+
+  def go
+    # TO DO : lance la partie
+    puts "Let's go !"
 
 
+  end
 
+  def turn
+
+    @yaya.show
+    puts "It's #{@p_one.pname}'s turn!"
+    @yaya.get_player_choice(@)
+
+    #TO DO : affiche le plateau, demande au joueur il joue quoi,
+    # vérifie si un joueur a gagné,
+    # passe au joueur suivant si la partie n'est pas finie
+  end
 
 end
 
@@ -46,13 +77,13 @@ class BoardCase
     @value = value
   end
 
-  def is_playable?(c_number)
-    if c_number.value.to_i == c_number.name
-      return true
-    else
-      return false
-    end
-  end
+  # def is_playable?(c_number)
+  #   if c_number.value.to_i == c_number.name
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
 
 end
 
@@ -60,7 +91,7 @@ class Board
 
   attr_accessor :cases, :choice
 
-  def initialize()
+  def initialize
     bc_1 = BoardCase.new(1, "1")
     bc_2 = BoardCase.new(2, "2")
     bc_3 = BoardCase.new(3, "3")
@@ -73,18 +104,9 @@ class Board
     @cases = [bc_1, bc_2, bc_3, bc_4, bc_5, bc_6, bc_7, bc_8, bc_9]
   end
 
-  def show_board()
-    # a = cases[0]
-    # b = cases[1]
-    # c = cases[2]
-    # d = cases[3]
-    # e = cases[4]
-    # f = cases[5]
-    # g = cases[6]
-    # h = cases[7]
-    # i = cases[8]
+  def show
 
-    puts "     |     |     "
+    puts "\n     |     |     "
     puts "  #{cases[0].value}  |  #{cases[1].value}  |  #{cases[2].value}  "
     puts "_____|_____|_____"
     puts "     |     |     "
@@ -93,44 +115,25 @@ class Board
     puts "     |     |     "
     puts "  #{cases[6].value}  |  #{cases[7].value}  |  #{cases[8].value}  "
     puts "     |     |     "
+
   end
 
   def get_player_choice
-    puts "\nSelect your next position :"
+
     @choice = (gets.chomp).to_i
     self.set_case_value(@choice, @pmark)
     self.show_board
   end
 
-  def set_case_value(c_number, pmark)
-    if is_playable?(c_number)
-      c_number.value = pmark
-    end
-  end
+  # def set_case_value(c_number, pmark)
+  #   if is_playable?(c_number)
+  #     c_number.value = pmark
+  #   end
+  # end
 
 end
 
-#
-# # Init du game
-# game = Game.new
+game = Game.new
 
-
-yaya = Board.new
-yaya.show_board()
-# yaya.set_case_value()
-p yaya.cases.class
-
-# Welcome !
-puts "Welcome to the BDX Tic-Tac-Toe ! Have fun with our progZ !"
-
-# Demande noms et Ajout des players dans le game
-
-# p_one = Player.new()
-# p_one.infos
-# p_two = Player.new()
-# p_two.infos
-# puts "Please, put the second player name :"
-# p_two = Player.new()
-#
-# puts p_one.info
-# puts p_two.info
+game.go
+game.turn
