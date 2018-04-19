cf31215c8a7a1455edc80167feb77f7e49acf43c
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-19 11:29:31 +0200

    cases

--- board.rb
+++ board.rb
@@ -4,12 +4,12 @@ class BoardCase
   attr_accessor :name, :value
 
   def initialize(name, value)
-    @name = name
+    @name = name.to_i
     @value = value
   end
 
   def is_playable?(c_number)
-    if c_number.value = c_number.name
+    if c_number.value.to_i == c_number.name
       return true
     else
       return false
@@ -24,47 +24,47 @@ class Board < BoardCase
   end
 
   def show_board(cases)
-    a = cases[0].value
-    b = cases[1].value
-    c = cases[2].value
-    d = cases[3].value
-    e = cases[4].value
-    f = cases[5].value
-    g = cases[6].value
-    h = cases[7].value
-    i = cases[8].value
+    a = cases[0]
+    b = cases[1]
+    c = cases[2]
+    d = cases[3]
+    e = cases[4]
+    f = cases[5]
+    g = cases[6]
+    h = cases[7]
+    i = cases[8]
 
     puts "     |     |     "
-    puts "  #{a}  |  #{b}  |  #{c}  "
+    puts "  #{a.value}  |  #{b.value}  |  #{c.value}  "
     puts "_____|_____|_____"
     puts "     |     |     "
-    puts "  #{d}  |  #{e}  |  #{f}  "
+    puts "  #{d.value}  |  #{e.value}  |  #{f.value}  "
     puts "_____|_____|_____"
     puts "     |     |     "
-    puts "  #{g}  |  #{h}  |  #{i}  "
+    puts "  #{g.value}  |  #{h.value}  |  #{i.value}  "
     puts "     |     |     "
   end
 
-  def set_case_value(c_number)
+  def set_case_value(c_number, pmark)
     if is_playable?(c_number)
-      c_number.value = @pmark
+      c_number.value = "#{pmark}"
     end
   end
 
 end
 
-bc_1 = BoardCase.new("a", "1")
-bc_2 = BoardCase.new("b", "2")
-bc_3 = BoardCase.new("c", "3")
-bc_4 = BoardCase.new("d", "4")
-bc_5 = BoardCase.new("e", "5")
-bc_6 = BoardCase.new("f", "6")
-bc_7 = BoardCase.new("g", "7")
-bc_8 = BoardCase.new("h", "8")
-bc_9 = BoardCase.new("i", "9")
+bc_1 = BoardCase.new(1, "1")
+bc_2 = BoardCase.new(2, "2")
+bc_3 = BoardCase.new(3, "3")
+bc_4 = BoardCase.new(4, "4")
+bc_5 = BoardCase.new(5, "5")
+bc_6 = BoardCase.new(6, "6")
+bc_7 = BoardCase.new(7, "7")
+bc_8 = BoardCase.new(8, "8")
+bc_9 = BoardCase.new(9, "9")
 
 cases = [bc_1, bc_2, bc_3, bc_4, bc_5, bc_6, bc_7, bc_8, bc_9]
 yaya = Board.new
 yaya.show_board(cases)
-yaya.set_case_value(bc_1)
+yaya.set_case_value(bc_1, "X")
 yaya.show_board(cases)
